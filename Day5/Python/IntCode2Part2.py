42526a297a831b28218e33bf94a445082eb404d9
Xychic <44938917+Xychic@users.noreply.github.com>
2019-12-05 11:51:24 +0000

    Add files via upload

--- Day5/Python/IntCode2Part2.py
+++ Day5/Python/IntCode2Part2.py
@@ -3,6 +3,7 @@ with open ("input.txt","r") as file:
 
 i = 0
 while True:
+    i %= len(code)
     op = code[i] % 100
     ad1 = (code[i] // 100) % 10
     ad2 = (code[i] // 1000) % 10
@@ -27,3 +28,21 @@ while True:
         p1 = code[i+1] if ad1==1 else code[code[i+1]]
         print("OUTPUT:",p1)
         i += 2
+    elif op == 5:
+        p1 = code[i+1] if ad1==1 else code[code[i+1]]
+        p2 = code[i+2] if ad2==1 else code[code[i+2]]
+        i = p2 if p1 == 1 else i+3
+    elif op == 6:
+        p1 = code[i+1] if ad1==1 else code[code[i+1]]
+        p2 = code[i+2] if ad2==1 else code[code[i+2]]
+        i = p2 if p1 == 0 else i+3
+    elif op == 7:
+        p1 = code[i+1] if ad1==1 else code[code[i+1]]
+        p2 = code[i+2] if ad2==1 else code[code[i+2]]
+        code[code[i+3]] = 1 if p1<p2 else 0
+        i += 4
+    elif op == 8:
+        p1 = code[i+1] if ad1==1 else code[code[i+1]]
+        p2 = code[i+2] if ad2==1 else code[code[i+2]]
+        code[code[i+3]] = 1 if p1==p2 else 0
+        i += 4
