5ecdc6ba233ec1747402e548118099645988acf1
Jacob Turner <44938917+Xychic@users.noreply.github.com>
2019-12-19 14:01:30 +0000

    Add files via upload
    Much nicer day today

---
+++ Day19/Python/tractorBeamPart2.py
@@ -0,0 +1,87 @@
+class IntComp():
+
+    def __init__(self, memory):
+        self.code = memory
+        self.overflow = {}
+        self.i = 0
+        self.rBase = 0
+            
+    def __getitem__(self, i):
+        if i < len(self.code):
+            return self.code[i]
+        else:
+            if i not in self.overflow:
+                self.overflow[i] = 0
+            return self.overflow[i]
+
+    
+    def __setitem__(self, i, item):
+        if i < len(self.code):
+            self.code[i] = item
+        else:
+            self.overflow[i] = item
+    
+    def index(self, pos):
+        addressMode = (self[self.i] // (10 * 10**pos)) % 10
+        if addressMode == 0:
+            return self[self.i+pos]
+        elif addressMode == 1:
+            return self.i + pos
+        elif addressMode == 2:
+            return self[self.i+pos] + self.rBase
+    
+    def val(self, pos):
+        return self[self.index(pos)]
+    
+    def run(self, inputs=[]):
+        out = []
+        while True:
+            op = self[self.i] % 100
+
+            if op == 1:
+                p1, p2, p3 = self.val(1), self.val(2), self.index(3)
+                self[p3] = p1 + p2
+                self.i += 4
+            elif op == 2:
+                p1, p2, p3 = self.val(1), self.val(2), self.index(3)
+                self[p3] = p1 * p2
+                self.i += 4
+            elif op == 3:
+                p1 = self.index(1)
+                self[p1] = inputs.pop(0)
+                self.i += 2
+            elif op == 4:
+                p1 = self.val(1)
+                self.i += 2
+                return p1
+            elif op == 5:
+                p1, p2 = self.val(1), self.val(2)
+                self.i = p2 if bool(p1) else self.i+3
+            elif op == 6:
+                p1, p2 = self.val(1), self.val(2)
+                self.i = p2 if not bool(p1) else self.i+3
+            elif op == 7:
+                p1, p2, p3 = self.val(1), self.val(2), self.index(3)
+                self[p3] = 1 if p1<p2 else 0
+                self.i += 4
+            elif op == 8:
+                p1, p2, p3 = self.val(1), self.val(2), self.index(3)
+                self[p3] = 1 if p1==p2 else 0
+                self.i += 4
+            elif op == 9:
+                p1 = self.val(1)
+                self.rBase += p1
+                self.i += 2
+            elif op == 99:
+                return None
+
+with open ("input.txt","r") as file:
+    code = [int(i) for i in file.read().split(",")]
+
+x = 0
+for y in range(100, 100000):
+    while not bool(IntComp(code.copy()).run([x,y])):
+        x += 1
+    if bool(IntComp(code.copy()).run([x+99,y-99])):
+        print(x*10000 + y-99)
+        break
