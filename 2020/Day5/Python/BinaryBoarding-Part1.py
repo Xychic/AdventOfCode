036a78729e7c1edc987f632344ae3896d5d53acf
Jacob Turner <jacob11turner@gmail.com>
2020-12-05 09:07:47 +0000

    Optimise Day 5 slightly

--- 2020/Day5/Python/BinaryBoarding-Part1.py
+++ 2020/Day5/Python/BinaryBoarding-Part1.py
@@ -8,8 +8,8 @@ def getSeatID(seat):
     seat = seat.replace("F","0").replace("B","1").replace("L", "0").replace("R","1")
     return int(seat, 2)
     
-best = 0
+seats = []
 for line in open(f"{sys.path[0]}/../input.txt").read().splitlines():
-    best = max(getSeatID(line), best)
+    seats.append(getSeatID(line))
 
-print(best)
+print(max(seats))
--- 2020/Day5/Python/BinaryBoarding-Part2.py
+++ 2020/Day5/Python/BinaryBoarding-Part2.py
@@ -8,16 +8,10 @@ def getSeatID(seat):
     seat = seat.replace("F","0").replace("B","1").replace("L", "0").replace("R","1")
     return int(seat, 2)
 
-allNum = [i for i in range(2**10)]
-
-high = 0
-low = float("inf")
+seats = []
 for line in open(f"{sys.path[0]}/../input.txt").read().splitlines():
-    sID = getSeatID(line)
-    high = max(sID, high)
-    low = min(sID, low)
-    allNum.remove(sID)
+    seats.append(getSeatID(line))
 
-ans = [x for x in allNum if (low < x < high)][0]
+allNum = [i for i in range(min(seats), max(seats))]
 
-print(ans)
+print([x for x in allNum if x not in seats][0])
