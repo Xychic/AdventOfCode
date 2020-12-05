afdf061483c491af9d7de8de0853c6129f989ab7
Jacob Turner <jacob11turner@gmail.com>
2020-12-05 07:57:32 +0000

    Refactor Haskell Day4

--- 2020/Day4/Haskell/PassportProcessing-Part1.hs
+++ 2020/Day4/Haskell/PassportProcessing-Part1.hs
@@ -6,8 +6,8 @@ import qualified Data.Text as Text
 
 printArray:: (Show a, Eq a) => [a] -> String
 printArray (x:xs)
-    | xs == [] = show x
-    | otherwise = (show x) ++ "\n" ++ printArray xs
+    | null xs = show x
+    | otherwise = show x ++ "\n" ++ printArray xs
 
 subsets:: Int -> [a] -> [[a]]
 subsets 0 _ = [[]]
@@ -15,20 +15,23 @@ subsets _ [] = []
 subsets n (x : xs) = map (x :) (subsets (n - 1) xs) ++ subsets n xs
 
 split:: Eq a => a -> [a] -> [[a]]
-split d [] = []
-split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
+split onChar [] = []
+split onChar toSplit = x : split onChar (drop 1 y) 
+    where (x,y) = span (/= onChar) toSplit
 
-groupPass:: [[Char]] -> [[Char]] -> [[Char]]
-groupPass groups remainder
-    | (length remainder) == 0 = []
-    | otherwise = groups ++ [unwords x] ++ (groupPass [] (drop ((length x) + 1) remainder)) where x = takeWhile(\x -> 0 < (length x)) remainder 
+groupPass:: [[Char]] -> [[Char]]
+groupPass remainder
+    | null remainder = []
+    | otherwise = unwords pass : groupPass (drop (length pass + 1) remainder)
+        where pass = takeWhile(not . null) remainder 
     
+needed:: [Text.Text]
 needed = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 main:: IO()
 main = do
     handle <- openFile "../input.txt" ReadMode
     contents <- hGetContents handle
-    let rows = (lines contents)
-    print (length (filter (\p -> (all (\n -> Text.isInfixOf n (Text.pack p)) needed)) (groupPass [] rows)))
+    let rows = lines contents
+    print (length (filter (\p -> all (\n -> Text.isInfixOf n (Text.pack p)) needed) (groupPass rows)))
     hClose handle
