204930ca5ce4ec25c79acebfee5fcdd7b8acb082
Jacob Turner <jacob11turner@gmail.com>
2020-12-06 10:04:33 +0000

    Day 6

---
+++ 2020/Day6/Haskell/CustomCustoms-Part1.hs
@@ -0,0 +1,40 @@
+import System.IO
+import Control.Monad
+import Data.List
+
+printArray:: (Show a, Eq a) => [a] -> String
+printArray (x:xs)
+    | null xs = show x
+    | otherwise = show x ++ "\n" ++ printArray xs
+
+subsets:: Int -> [a] -> [[a]]
+subsets 0 _ = [[]]
+subsets _ [] = []
+subsets n (x : xs) = map (x :) (subsets (n - 1) xs) ++ subsets n xs
+
+split:: Eq a => a -> [a] -> [[a]]
+split onChar [] = []
+split onChar toSplit = x : split onChar (drop 1 y) 
+    where (x,y) = span (/= onChar) toSplit
+
+replace:: (Eq a) => [a] -> a -> a -> [a]
+replace [] _ _ = []
+replace (x:xs) a b 
+    | x == a = b : replace xs a b
+    | otherwise = x : replace xs a b
+
+groupQuestions:: [[Char]] -> [[Char]]
+groupQuestions remainder
+    | null remainder = []
+    | otherwise = concat pass : groupQuestions (drop (length pass + 1) remainder)
+        where pass = takeWhile(not . null) remainder 
+
+main:: IO()
+main = do
+    handle <- openFile "../input.txt" ReadMode
+    contents <- hGetContents handle
+    let rows = lines contents
+    let groups = map (length . nub) (groupQuestions rows)
+    print (sum groups)
+
+    hClose handle
---
+++ 2020/Day6/Python/CustomCustoms-Part2.py
@@ -0,0 +1,18 @@
+import sys
+import collections
+import itertools
+import numpy as np
+import string
+
+questions = set(string.ascii_lowercase)
+groups = []
+
+for line in open(f"{sys.path[0]}/../input.txt").read().splitlines():
+    if len(line):
+        questions = questions & set(line)
+    else:
+        groups.append(questions)
+        questions = set(string.ascii_lowercase)
+groups.append(questions)
+
+print(sum([len(g) for g in groups]))
