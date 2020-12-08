f9be732dbc1e6131750c77255c7a114ed954e08c
Jacob Turner <jacob11turner@gmail.com>
2020-12-08 10:40:36 +0000

    Remove unused code

--- 2020/Day5/Haskell/BinaryBoarding-Part1.hs
+++ 2020/Day5/Haskell/BinaryBoarding-Part1.hs
@@ -6,16 +6,6 @@ printArray (x:xs)
     | null xs = show x
     | otherwise = show x ++ "\n" ++ printArray xs
 
-subsets:: Int -> [a] -> [[a]]
-subsets 0 _ = [[]]
-subsets _ [] = []
-subsets n (x : xs) = map (x :) (subsets (n - 1) xs) ++ subsets n xs
-
-split:: Eq a => a -> [a] -> [[a]]
-split onChar [] = []
-split onChar toSplit = x : split onChar (drop 1 y) 
-    where (x,y) = span (/= onChar) toSplit
-
 replace:: (Eq a) => [a] -> a -> a -> [a]
 replace [] _ _ = []
 replace (x:xs) a b 
