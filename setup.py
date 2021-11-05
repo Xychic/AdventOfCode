6648d69b3721708bf39c3b3db1612a17a0d46265
Jacob Turner <jacob11turner@gmail.com>
2021-11-05 16:25:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -137,19 +137,19 @@ main = do
 TEMPLATE_DICT["Rust"] = """use std::fs;
 
 fn main() {
-    let input: String = fs::read_to_string("../../../input.txt").expect("error reading file");
-    let lines: Vec<&str> = input.trim().split("\\n").collect();
+    let input = fs::read_to_string("../../../input.txt").expect("error reading file");
+    let input = input.trim();
 
-    println!("Part 1: {}", part_1(lines.clone()));
-    println!("Part 2: {}", part_2(lines.clone()));
+    println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
-fn part_1(lines: Vec<&str>) -> usize {
-    0
+fn part_1(input: &str) -> usize {
+    todo!()
 }
 
-fn part_2(lines: Vec<&str>) -> usize {
-    0
+fn part_2(input: &str) -> usize {
+    todo!()
 }
 """
 
