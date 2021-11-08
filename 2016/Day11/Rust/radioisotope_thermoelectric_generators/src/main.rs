7d3b519d7bd7abcf631d564cb2cd44b4fa4ba1d7
Jacob Turner <jacob11turner@gmail.com>
2021-11-08 21:00:33 +0000

    Finish 2016

--- 2016/Day11/Rust/radioisotope_thermoelectric_generators/src/main.rs
+++ 2016/Day11/Rust/radioisotope_thermoelectric_generators/src/main.rs
@@ -1,17 +1,118 @@
-use std::fs;
+use std::{cmp::Ordering, fs};
+
+use itertools::Itertools;
+
+type FloorState = (usize, Vec<Vec<isize>>);
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+struct State {
+    cost: usize,
+    state: FloorState,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 fn main() {
-    let input: String = fs::read_to_string("../../../input.txt").expect("error reading file");
-    let lines: Vec<&str> = input.trim().split("\n").collect();
+    let input = fs::read_to_string("../../../input.txt").expect("error reading file");
+//     let input = "The first floor contains a strontium-compatible microchip, and a plutonium-compatible microchip.
+// The second floor contains a strontium generator.
+// The third floor contains a plutonium generator.
+// The fourth floor contains nothing relevant.";
+    let input: Vec<_> = input.trim().split('\n').map(|x| parse_line(x)).collect();
 
-    println!("Part 1: {}", part_1(lines.clone()));
-    println!("Part 2: {}", part_2(lines.clone()));
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(lines: Vec<&str>) -> usize {
-    0
+fn part_1(lines: &[Vec<isize>]) -> usize {
+    let mut floors: Vec<_> = lines.iter().map(|x| x.len()).collect();
+    let mut total = 0;
+    for floor in 0..floors.len()-1 {
+        total += 2 * floors[floor] - 3;
+        floors[floor+1] += floors[floor];
+    }
+
+    total
 }
 
-fn part_2(lines: Vec<&str>) -> usize {
-    0
+fn part_2(lines: &[Vec<isize>]) -> usize {
+    let mut floors: Vec<_> = lines.iter().map(|x| x.len()).collect();
+    let mut total = 0;
+    floors[0] += 4;
+    for floor in 0..floors.len()-1 {
+        total += 2 * floors[floor] - 3;
+        floors[floor+1] += floors[floor];
+    }
+
+    total
+}
+
+fn parse_line(line: &str) -> Vec<isize> {
+    let res = Vec::new();
+    if line.contains("nothing relevant") { return res; }
+    let line = line.replace(" and a", " a").replace(".", "");
+    let line = line.split(' ').skip(5).collect::<Vec<_>>().join(" ");
+    let mut line: Vec<_> = line.split(", a ").map(|x| {
+        match x {
+            "strontium generator" => -1,
+            "strontium-compatible microchip" => 1,
+            "plutonium generator" => -2,
+            "plutonium-compatible microchip" => 2,
+            "thulium generator" => -3,
+            "thulium-compatible microchip" => 3,
+            "ruthenium generator" => -4,
+            "ruthenium-compatible microchip" => 4,
+            "curium generator" => -5,
+            "curium-compatible microchip" => 5,
+            _ => panic!("Unknown item \"{}\"", x)
+        }
+    }).collect();
+    line.sort();
+    line
+}
+
+fn _is_safe(floor: &[isize]) -> bool {
+    floor.iter().min().unwrap_or(&1) > &0 ||
+    floor.iter().all(|&x| x < 0 || floor.contains(&-x))
+}
+
+fn _get_next_states(state: &FloorState) -> Vec<FloorState> {
+    let mut states = Vec::new();
+    let (floor, floors) = state;
+    let mut moves: Vec<_> = floors[*floor].iter().combinations(2).unique().collect();
+    moves.extend(floors[*floor].iter().map(|x| vec![x]));
+
+    let mut directions = Vec::new();
+    if floor < &floors.len() { directions.push(1); }
+    if floor != &0 && !floors[floor-1].is_empty() { directions.push(-1); }
+
+    for m in moves {
+        for d in &directions {
+            let mut new_floors = floors.clone();
+            let new_floor = (*floor as isize + *d) as usize;
+            for &item in &m {
+                new_floors[new_floor].push(*item);
+                let index = new_floors[*floor].iter().position(|x| *x == *item).unwrap();
+                new_floors[*floor].remove(index);
+            }
+            if ! _is_safe(&new_floors[*floor]) || ! _is_safe(&new_floors[new_floor]) {
+                continue;
+            }
+            new_floors[new_floor].sort();
+            new_floors[*floor].sort();
+            states.push((new_floor, new_floors));
+        }
+    }
+
+    states
 }
---
+++ 2016/Day24/Rust/air_duct_spelunking/src/main.rs
@@ -0,0 +1,149 @@
+use std::{cmp::Ordering, collections::{BinaryHeap, HashMap}, fs};
+
+use itertools::Itertools;
+
+type Node = (usize, usize);
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+struct State {
+    cost: usize,
+    position: Node,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("../../../input.txt").expect("error reading file");
+    let input: Vec<Vec<_>> = input.trim().split('\n').map(|x| x.chars().into_iter().collect()).collect();
+
+    let mut targets = Vec::new();
+    for (y, line) in input.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if c != &'#' && c != &'.' {
+                targets.push((*c, (x, y)));
+            }
+        }
+    }
+    targets.sort_by(|(a, _), (b,_)| a.cmp(b));
+    let targets: Vec<_> = targets.iter().map(|&(_, a)| a).collect();
+
+    let mut distances = HashMap::new();
+    let grid = make_grid(&input);
+    for (i, &a) in targets.iter().enumerate() {
+        for &b in targets.iter().skip(i+1) {
+            let dist = get_dist(&grid, a, b);
+            distances.insert((a, b), dist);
+            distances.insert((b, a), dist);
+        }
+    }
+
+    println!("Part 1: {}", part_1(&targets, &distances));
+    println!("Part 2: {}", part_2(&targets, &distances));
+}
+
+fn part_1(targets: &[Node], distances: &HashMap<(Node, Node), usize>) -> usize {
+    targets.iter().skip(1).permutations(targets.len()-1).unique().map(|mut perm| {
+        perm.insert(0, &targets[0]);
+        let mut cost = 0;
+        for (&a, &b) in perm.iter().zip(perm.iter().skip(1)) {
+            cost += distances.get(&(*a, *b)).unwrap();
+        }
+        cost
+    })
+    .min()
+    .unwrap()
+}
+
+fn part_2(targets: &[Node], distances: &HashMap<(Node, Node), usize>) -> usize {
+    targets.iter().skip(1).permutations(targets.len()-1).unique().map(|mut perm| {
+        perm.insert(0, &targets[0]);
+        perm.push(&targets[0]);
+        let mut cost = 0;
+        for (&&a, &&b) in perm.iter().zip(perm.iter().skip(1)) {
+            cost += distances.get(&(a, b)).unwrap();
+        }
+        cost
+    })
+    .min()
+    .unwrap()
+}
+
+fn make_grid(input: &[Vec<char>]) -> Vec<Vec<bool>> {
+    let mut grid = vec![vec![false; input[0].len()]; input.len()];
+    for (y, line) in input.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            grid[y][x] = c != &'#';
+        }
+    }
+    grid
+}
+
+fn _print_grid(grid: &[Vec<bool>]) {
+    for row in grid {
+        for square in row {
+            print!("{}", if *square {'.'} else {'#'})
+        }
+        println!();
+    }
+}
+
+fn get_dist(grid: &[Vec<bool>], a: Node, b: Node) -> usize {
+    let mut distances: HashMap<Node, usize> = HashMap::new();
+    let mut heap: BinaryHeap<State> = BinaryHeap::new();
+    let mut path: HashMap<Node, Node> = HashMap::new();
+    
+    let start = a;
+    let end = b;
+
+    distances.insert(start, 0);
+    heap.push(State {cost: 0, position: start});
+
+    while let Some(State {cost, position}) = heap.pop() {
+        if position == end { 
+            return cost; 
+        }
+
+        if &cost > distances.get(&position).unwrap_or(&usize::MAX) { continue; }
+
+        for edge in get_neighbours(grid, position) {
+            let next = State{cost: cost + 1, position: edge};
+
+            if &next.cost < distances.get(&edge).unwrap_or(&usize::MAX) {
+                path.insert(next.position, position);
+                heap.push(next);
+                distances.insert(next.position, next.cost);
+            }
+        }
+    }
+    panic!("No path from {:?} to {:?} found!", a, b)
+}
+
+fn get_neighbours(grid: &[Vec<bool>], (x, y): Node) -> Vec<Node> {
+    let mut neighbours: Vec<Node> = Vec::new();
+    let dirs: Vec<(isize, isize)> = vec![(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+    for (dx, dy) in dirs {
+        let temp_x = x as isize + dx;
+        let temp_y = y as isize + dy;
+
+        if temp_x.is_negative() || temp_y.is_negative() || temp_x >= grid[0].len() as isize || temp_y >= grid.len() as isize {
+            continue;
+        }
+        if grid[temp_y as usize][temp_x as usize] {
+            neighbours.push((temp_x as usize, temp_y as usize));
+        }
+
+    }
+    neighbours
+}
