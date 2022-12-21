0abe188a492ffd16c14200adaeef52cbf2718128
Jacob Turner <jacob11turner@gmail.com>
2022-12-21 11:59:45 +0000

    2022 Day 21

---
+++ 2022/Day21/Rust/monkey_math/src/main.rs
@@ -0,0 +1,55 @@
+use monkey_math::*;
+use std::{fs, time::Instant};
+
+fn main() {
+    let start = Instant::now();
+    let raw_input = fs::read_to_string("../../../input.txt").expect("error reading file");
+    let input = parse(&raw_input);
+    println!("Parsed input in {:?}", Instant::now() - start);
+
+    let start = Instant::now();
+    println!(
+        "Part 1: {}, took {:?}",
+        part_1(&input),
+        Instant::now() - start
+    );
+    let start = Instant::now();
+    println!(
+        "Part 2: {}, took {:?}",
+        part_2(&input),
+        Instant::now() - start
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_1_INPUT: &str = "root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32";
+    const TEST_1_ANSWER: usize = 152;
+    const TEST_2_INPUT: &str = TEST_1_INPUT;
+    const TEST_2_ANSWER: usize = 301;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse(&TEST_1_INPUT)), TEST_1_ANSWER);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&parse(&TEST_2_INPUT)), TEST_2_ANSWER);
+    }
+}
