b190e4961bfb64e8fe63693cd2e34f68f3529641
Jacob Turner <jacob11turner@gmail.com>
2023-12-01 16:52:57 +0000

    Optimise part 2 solution

--- 2023/Day01/Rust/trebuchet/benches/bench_trebuchet.rs
+++ 2023/Day01/Rust/trebuchet/benches/bench_trebuchet.rs
@@ -9,8 +9,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let raw_input = fs::read_to_string("../../input.txt").expect("error reading file");
     let input = parse(&raw_input);
     c.bench_function("2023::Day01::parse", |b| b.iter(|| parse(&raw_input)));
-    c.bench_function("2023::Day01::part 1", |b| b.iter(|| part_1(&input)));
-    c.bench_function("2023::Day01::part 2", |b| b.iter(|| part_2(&input)));
+    c.bench_function("2023::Day01::part 1", |b| b.iter(|| part_1(input)));
+    c.bench_function("2023::Day01::part 2", |b| b.iter(|| part_2(input)));
 }
 
 criterion_group!(benches, criterion_benchmark);
