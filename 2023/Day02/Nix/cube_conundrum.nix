48a7e8842774162cb941003132d17ef8c2dd95d5
Jacob Turner <jacob11turner@gmail.com>
2023-12-03 08:55:39 +0000

    2023 Day 02 Nix

--- 2023/Day02/Nix/cube_conundrum.nix
+++ 2023/Day02/Nix/cube_conundrum.nix
@@ -9,14 +9,20 @@ in rec {
         split: { 
           id = lib.strings.toInt(
             builtins.elemAt (
-              lib.strings.splitString " " (builtins.elemAt split 0)
+              lib.strings.splitString " " (builtins.head split)
             ) 1
           );
-          val = {
-            red = 0;
-            green = 0;
-            blue = 0;
-          };
+          cubes = builtins.foldl' (acc: x: (data: 
+            if data.colour == "red" then 
+              {red=lib.max acc.red data.count; green=acc.green; blue=acc.blue;}
+            else if data.colour == "green" then
+              {red=acc.red; green=lib.max acc.green data.count; blue=acc.blue;}
+            else 
+              {red=acc.red; green=acc.green; blue=lib.max acc.blue data.count;}
+          )(
+            {count = lib.strings.toInt(builtins.head x); colour = builtins.elemAt x 1;})) 
+            {red=0; green=0; blue=0;} 
+            (map (lib.strings.splitString " ") (lib.strings.splitString ", " (lib.strings.replaceStrings [";"] [","] (builtins.elemAt split 1))));
         }
       )(lib.strings.splitString ": " line)
   )(
@@ -24,7 +30,7 @@ in rec {
   );
   parsed = parse (builtins.readFile ../input.txt);
   
-  part1' = input: input;
+  part1' = input: builtins.foldl' builtins.add 0 (map (x: x.id) (lib.filter (x: x.cubes.red <= 12 && x.cubes.green <= 13 && x.cubes.blue <= 14) input));
   part1 = input: (
     lib.fix (
       self: {
@@ -34,7 +40,7 @@ in rec {
     )
   ).result;
 
-  part2' = input: input;
+  part2' = input: builtins.foldl' (acc: x: acc + (x.cubes.red * x.cubes.green * x.cubes.blue)) 0 input;
   part2 = input: (
     lib.fix (
       self: {
@@ -49,9 +55,9 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n";
-  TEST_1_ANSWER = 0;
+  TEST_1_ANSWER = 8;
   TEST_2_INPUT = TEST_1_INPUT;
-  TEST_2_ANSWER = 0;
+  TEST_2_ANSWER = 2286;
 
 
   result = {part1 = part1 parsed; part2 = part2 parsed;};
