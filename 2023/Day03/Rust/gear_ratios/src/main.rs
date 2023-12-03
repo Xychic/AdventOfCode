1d79b557d8b176a694103de50827335b640c0abc
Jacob Turner <jacob11turner@gmail.com>
2023-12-03 08:27:39 +0000

    2023 Day 03

---
+++ 2023/Day03/Rust/gear_ratios/src/main.rs
@@ -0,0 +1,42 @@
+use gear_ratios::{parse, part_1, part_2};
+use std::{fs, time::Instant};
+
+fn main() {
+    let mut start = Instant::now();
+    let raw_input = fs::read_to_string("../../../input.txt").expect("error reading file");
+    let input = parse(&raw_input);
+    println!("Parsed input in {:?}", start.elapsed());
+
+    start = Instant::now();
+    println!("Part 1: {}, took {:?}", part_1(&input), start.elapsed());
+    start = Instant::now();
+    println!("Part 2: {}, took {:?}", part_2(&input), start.elapsed());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_1_INPUT: &str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+    const TEST_1_ANSWER: usize = 4361;
+    const TEST_2_INPUT: &str = TEST_1_INPUT;
+    const TEST_2_ANSWER: usize = 467_835;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse(TEST_1_INPUT)), TEST_1_ANSWER);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&parse(TEST_2_INPUT)), TEST_2_ANSWER);
+    }
+}
