3def3f896bc5d607d5881eb84aaf4c896cc290ef
Jacob Turner <jacob11turner@gmail.com>
2023-12-04 10:13:00 +0000

    2023 Day 04

---
+++ 2023/Day04/Rust/scratchcards/src/main.rs
@@ -0,0 +1,38 @@
+use scratchcards::{parse, part_1, part_2};
+use std::{fs, time::Instant};
+
+fn main() {
+    let mut start = Instant::now();
+    let raw_input = fs::read_to_string("../../../input.txt").expect("error reading file");
+    let input = parse(&raw_input);
+    println!("Parsed input in {:?}", start.elapsed());
+
+    start = Instant::now();
+    println!("Part 1: {}, took {:?}", part_1(&input), start.elapsed());
+    start = Instant::now();
+    println!("Part 2: {}, took {:?}", part_2(&input), start.elapsed());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_1_INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+    const TEST_1_ANSWER: usize = 13;
+    const TEST_2_INPUT: &str = TEST_1_INPUT;
+    const TEST_2_ANSWER: usize = 30;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse(TEST_1_INPUT)), TEST_1_ANSWER);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&parse(TEST_2_INPUT)), TEST_2_ANSWER);
+    }
+}
